42d05b04a91ef59c8a42d62f612db9ccb67e68b8
hemomaster <hemomaster@gmail.com>
2021-05-08 08:22:41 +0300

    Сверстал и стилизовал header, секции: hero, lesson, stories, videos slider

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const swiper = new Swiper(".slider__container", {
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    spaceBetween: 100,
+  });
+});
