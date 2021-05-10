9529d37537563a28d413a4c0dc4135b97bcdf95e
hemomaster <hemomaster@gmail.com>
2021-05-10 10:14:38 +0300

    Сверстал, стилизовал навигацию. Настроил скрипт js

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,24 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const swiper = new Swiper(".slider__container", {
+  const sliderContainer = document.querySelector(".slider__container"),
+    nav = document.getElementById("nav"),
+    burger = document.getElementById("burger");
+
+  const swiper = new Swiper(sliderContainer, {
     centeredSlides: true,
     slidesPerView: "auto",
     loop: true,
     spaceBetween: 100,
+  });
+
+  burger.addEventListener("click", () => nav.classList.add("active"));
+
+  nav.addEventListener("click", (e) => {
+    const target = e.target;
+
+    if (target.classList.contains("nav__close")) nav.classList.remove("active");
+    if (target.classList.contains("nav__list-link"))
+      nav.classList.remove("active");
   });
 });
