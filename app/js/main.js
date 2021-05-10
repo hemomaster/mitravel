b0a00d0f34a397657d79e48d27a5a2a045c62654
hemomaster <hemomaster@gmail.com>
2021-05-10 13:06:31 +0300

    Настроил воспроизведение видео для слайдера секции hero

--- app/js/main.js
+++ app/js/main.js
@@ -2,15 +2,43 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   const sliderContainer = document.querySelector(".slider__container"),
+    mainSlider = document.querySelector(".main-slider"),
     nav = document.getElementById("nav"),
-    burger = document.getElementById("burger");
+    burger = document.getElementById("burger"),
+    videoPlayBtns = document.querySelectorAll(".main-slider__media-btn"),
+    videos = document.querySelectorAll(".main-slider__media-video");
 
-  const swiper = new Swiper(sliderContainer, {
+  // swiper 1
+  const swiperMainSlider = new Swiper(mainSlider, {
+    slidesPerView: 1,
+    loop: true,
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true,
+    },
+    navigation: {
+      nextEl: ".main-slider__btn.btn-next",
+      prevEl: ".main-slider__btn.btn-prev",
+    },
+  });
+
+  swiperMainSlider.on("slideChange", () =>
+    videos.forEach((video, index) => {
+      video.pause();
+      video.currentTime = 0;
+      videoPlayBtns[index].style.display = "flex";
+    })
+  );
+
+  // swiper 2
+  const swiperSliderContainer = new Swiper(sliderContainer, {
     centeredSlides: true,
     slidesPerView: "auto",
     loop: true,
     spaceBetween: 100,
   });
+
+  // navigation menu
 
   burger.addEventListener("click", () => nav.classList.add("active"));
 
@@ -21,4 +49,15 @@ document.addEventListener("DOMContentLoaded", () => {
     if (target.classList.contains("nav__list-link"))
       nav.classList.remove("active");
   });
+
+  videoPlayBtns.forEach((btn, index) =>
+    btn.addEventListener("click", (e) => {
+      btn.style.display = "none";
+      videos[index].play();
+    })
+  );
+
+  // inputMask
+
+  // validate forms
 });
